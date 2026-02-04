99f082bf5d7ab5e79df9bfdddea410d2c8e59512
Rq <iwliluqp@gmail.com>
2026-02-04 14:20:47 +0300

    Initial Commit

---
+++ tr/trApp.swift
@@ -0,0 +1,17 @@
+//
+//  trApp.swift
+//  tr
+//
+//  Created by Rama AlQahtani on 16/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct trApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
